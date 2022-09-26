680b2eaba798c58ba4b16ed03b4d2ffe5cf9e572
Dmitry Lichmanov <dimlichmanov@gmail.com>
2022-09-26 17:26:56 +0300

    Done task1 with dynamic memory

--- 01_GitBuildReq/main.c
+++ 01_GitBuildReq/main.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
+#include "stdlib.h"
 #include <string.h>
 
+#define BUF_SIZE 10
+#define INIT_SIZE 25
 
 int main() {
-    char buffer[100];
+    char* buffer = malloc(INIT_SIZE * sizeof(char));
+    char temp_buffer[BUF_SIZE];
+    size_t cur_len = 0, all_len = INIT_SIZE, temp_len;
 
-    scanf("%s", buffer);
+    do {
+        fgets(temp_buffer,BUF_SIZE,stdin);
+        temp_len = strlen(temp_buffer);
 
-    printf("Hello, %s\n", buffer);
+        if (cur_len + temp_len > all_len) {
+            all_len *= 2;
+            buffer = realloc(buffer, all_len);
+        }
+
+        strcpy(buffer + cur_len, temp_buffer);
+        cur_len += temp_len;
+
+    } while (temp_len == BUF_SIZE - 1 && temp_buffer[BUF_SIZE - 2] != '\n');
+
+    printf("Hello, %s", buffer);
     return 0;
 }
