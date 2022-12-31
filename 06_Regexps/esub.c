5bfb48f89928662fcefe3c8f72830c743186a7e1
Dmitry Lichmanov <dimlichmanov@gmail.com>
2022-12-31 20:06:52 +0300

    06 task REdone

--- 06_Regexps/esub.c
+++ 06_Regexps/esub.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include "string.h"
 #include <ctype.h>
+#include "math.h"
 #include <stdlib.h>
 #include "stdbool.h"
 #define MAX_POCKET 20
 
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
 void print_pocket(int begin, int end, char * string) {
     for (int i = begin; i < end; i++) {
@@ -21,23 +23,27 @@ int main(int argc, char **argv) {
 
     char *regexp = argv[1], *substitution = argv[2], *string = argv[3];
     regex_t regex;
+    char errbuf[100];
+    int err;
 
-    int err = regcomp(&regex, regexp, REG_EXTENDED);
-    if (err != 0){
-        printf("error in regexp");
-    }
+    if (0 != (err = regcomp(&regex, regexp, REG_EXTENDED))) {
+      regerror(err, &regex, errbuf, 100);
+      printf("regcomp() failed with '%s'\n", errbuf);
+      exit(EXIT_FAILURE);
+   }
 
     regmatch_t pm[MAX_POCKET];
     err = regexec(&regex, string, MAX_POCKET, pm, 0);
-    if (err != 0){
-        printf("error in exec");
-    }
 
-    for (int i = 0; i < pm[0].rm_so; i++) {
+    for (int i = 0; i < MIN(pm[0].rm_so, strlen(string)); i++) {
         putc(string[i], stdout);
+    }
+    if (err == REG_NOMATCH) {
+        putc('\n', stdout);
     }
 
     bool esc_status = false;
+    if (err != REG_NOMATCH) {
     for (char *str = substitution; *str != '\0'; str++) {
         if (*str=='\\' && !esc_status) {
             esc_status = 1;
@@ -67,7 +73,6 @@ int main(int argc, char **argv) {
                 case '\"':
                     putc('\"', stdout);
                     continue;
-
                 case 'a':
                     putc('\a', stdout);
                     continue;
@@ -98,15 +103,16 @@ int main(int argc, char **argv) {
     }
 
     if (esc_status) {
-        printf("\nBAD SUBSTITURE, abort...\n");
+        printf("\nBAD SUBSTITUTE, abort...\n");
         regfree(&regex);
         return -1;
     }
+    
     for (int i = pm[0].rm_eo; i < strlen(string); i++) {
         putc(string[i], stdout);
     }
     putc('\n',stdout);
-
+    }
     regfree(&regex);
     return 0;
 
